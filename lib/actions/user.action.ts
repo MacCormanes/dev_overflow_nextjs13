56966bcb6dff0716bfb32263a9a791ba167a0b09
MacCormanes <janmarc132@gmail.com>
2023-10-07 18:51:04 +0800

    fixed some errors for deployment

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -69,6 +69,7 @@ export async function deleteUser(params: DeleteUserParams) {
     // and questions, answers, comments, etc
 
     // get user question ids
+    // eslint-disable-next-line no-unused-vars
     const userQuestionIds = await Question.find({ author: user._id }).distinct(
       "_id"
     );
